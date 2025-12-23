00ff0d305d4e6ef385f7ac452dc609328d965605
GeorgeR04 <rotaru.george03@gmail.com>
2025-12-23 14:22:51 +0100

    v2.0.0: Dockerize frontend and update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
-        'fade-slide': 'fadeSlide 0.3s ease-out',
+        "fade-slide": "fadeSlide 0.3s ease-out"
       },
       keyframes: {
         fadeSlide: {
-          '0%': { opacity: 0, transform: 'translateY(-10%)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
-        },
-      },
+          "0%": { opacity: 0, transform: "translateY(-10%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" }
+        }
+      }
     }
   },
-  plugins: [],
-}
+  plugins: []
+};
